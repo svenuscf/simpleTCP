4540f28e4bc00878c09fddac98acbfd7e420d5db
svenuscf <ajidom@gmail.com>
2018-07-16 08:35:58 +0800

    Version 1.01
    Removed all trash codes

--- simpleTCPclient.java
+++ simpleTCPclient.java
@@ -19,51 +19,24 @@ import static java.lang.System.out;
 public class simpleTCPclient extends JFrame
 {
   private static java.util.Timer timer, rpkt_thread;
-  private static final int IP_PKT_SIZE_MAX = 1518;
-  private static int pps=0;
   private static String group = "127.0.0.1";
   private static String log = "";
   private static int port = 5000;
-  private static int check_seqno=0, seqno=0, totalPktLost=0, totalPktRcvd=0;
-  private static boolean receiving = false, OUT_SEQ = false;
+  private static boolean receiving = false;
   private static Color RED, GREEN;
 
-  private static JTextField t_pps, t_pktloss, t_pktrcvd, t_group, t_port;
+  private static JTextField t_group, t_port;
   private static JTextArea messageLog;
   private static JButton start, stop, reset, clear; 
-  private static JLabel l_pps, l_pktloss, l_pktrcvd, l_group, l_port, l_int;
-  private static JComboBox n_interface;
+  private static JLabel l_group, l_port;
 
-  private static MulticastSocket s;
-  private static byte [] buf;
   private static Socket clientSocket;
-  private static DatagramPacket p;
-  private static NetworkInterface netint;
+
 
   public simpleTCPclient()
   {
     super ("Simple TCP Client 1.0");
     setIconImage ( new ImageIcon("mcast.gif").getImage());
-    
-    String[] netString = {""};
-    JComboBox<String> n_interface = new JComboBox<String> (netString);
-    
-    /*
-    buf = new byte [IP_PKT_SIZE_MAX];
-
-    // List out available Network Interfaces  
-    try
-    {
-      Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-      for (NetworkInterface netint : Collections.list(nets))
-      {
-        //out.printf("Name: %s\n", netint.getName());
-        n_interface.addItem(netint.getName());
-      }
-    } catch  ( Exception ex ) 
-    {
-    }
-    */
 
     class ButtonListener implements ActionListener
     {
@@ -86,95 +59,32 @@ public class simpleTCPclient extends JFrame
                BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
                clientSocket = new Socket(group, port);
              } catch ( Exception ex ) {}
-             /*
-             group = t_group.getText(); 
-             port = Integer.parseInt(t_port.getText());
-             if (port < 1024 || port > 65535) 
-             { port = 5000; t_port.setText("5000"); }
-             start.setEnabled(false);
-             stop.setEnabled(true);
-             Calendar now = Calendar.getInstance();
-             log += "Multicast Started at: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
-             messageLog.setText(log);
-             try
-             {
-               s = new MulticastSocket(port);
 
-               Enumeration<NetworkInterface> nic = NetworkInterface.getNetworkInterfaces();
-               for (NetworkInterface nc : Collections.list(nic))
-               {
-                 String x = String.valueOf(n_interface.getSelectedItem());
-                 if ( x.equals (nc.getName()))
-	           s.setNetworkInterface(nc);
-               }
-               s.joinGroup(InetAddress.getByName(group));
-             } catch ( Exception ex ) {}
-
-             timer = new java.util.Timer();
-             rpkt_thread = new java.util.Timer();
-             timer.scheduleAtFixedRate(new Schedule(), 0, 1000);
-             rpkt_thread.scheduleAtFixedRate(new Rpkt(), 0, 1);
-             receiving = true;
-             */
           }
           else if ("Close".equals(e.getActionCommand()))
           {
-             /*
-             receiving = false;
-             start.setEnabled(true);
-             stop.setEnabled(false);
-             */
              try
              {
                 clientSocket.close();
                 receiving = false;
                 start.setEnabled(true);
                 stop.setEnabled(false);
-               // s.leaveGroup(InetAddress.getByName(group));
+
              } catch ( Exception ex ) {}
 
-             /*
-             Calendar now = Calendar.getInstance();
-             log += "Multicast Stopped at: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
-             messageLog.setText(log);
-             t_pps.setBackground ( Color.lightGray );
-             messageLog.setBackground ( Color.lightGray );
-             t_pktloss.setBackground ( Color.lightGray );
-             t_pktrcvd.setBackground ( Color.lightGray );
-             check_seqno = 0;
-             seqno = 0;
-             timer.cancel();
-             rpkt_thread.cancel();
-             */
           }
           else if ("Send".equals(e.getActionCommand()))
           {
              try
              { 
                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-             // outToServer.writeBytes(sentence + 'n');
                outToServer.writeBytes(messageLog.getText() + 'n');
              } catch ( Exception ex ) {}
-             /*
-             check_seqno = 0;
-             seqno = 0;
-             Calendar now = Calendar.getInstance();
-             log += "Sequence number reset at: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
-             messageLog.setText(log);
-             */
+
           }
-          else 		// clear
+          else 		//Send timestamp
           {
-             /*
-             check_seqno = 0;
-             seqno = 0;
-             totalPktLost = 0;
-             totalPktRcvd = 0;
-             t_pps.setBackground ( Color.lightGray );
-             messageLog.setBackground ( Color.lightGray );
-             t_pktloss.setBackground ( Color.lightGray );
-             t_pktrcvd.setBackground ( Color.lightGray );
-             */
+ 
              Calendar now = Calendar.getInstance();
              log = "New timestamp at: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
              messageLog.setText(log);
@@ -207,22 +117,9 @@ public class simpleTCPclient extends JFrame
     
     JPanel panel = new JPanel ( new GridLayout (2, 2) );
 
-    /*
-    l_pps = new JLabel("Rate (pps)");
-    l_pktloss = new JLabel("Packet loss");
-    l_pktrcvd = new JLabel("Packet received");
-     */
     l_group = new JLabel("TCP Server Host");
     l_port = new JLabel("Port");
-    /*
-    l_int = new JLabel("Interface");
-    t_pps = new JTextField("0");
-    t_pps.setEditable(false);
-    t_pktloss = new JTextField("0");
-    t_pktloss.setEditable(false);
-    t_pktrcvd = new JTextField("0");
-    t_pktrcvd.setEditable(false);
-    */
+
     t_group = new JTextField("127.0.0.1");
     t_port = new JTextField("5000");
   
@@ -231,16 +128,6 @@ public class simpleTCPclient extends JFrame
     panel.add(l_port);
     panel.add(t_port);
     
-    /*
-    panel.add(l_pktrcvd);
-    panel.add(t_pktrcvd);
-    panel.add(l_pktloss);
-    panel.add(t_pktloss);
-    panel.add(l_pps);
-    panel.add(t_pps);
-    panel.add(l_int);
-    panel.add(n_interface);
-    */
     JPanel button = new JPanel ();
     button.setLayout( new FlowLayout(FlowLayout.CENTER));
 
@@ -269,141 +156,15 @@ public class simpleTCPclient extends JFrame
     cp.add (logpane, BorderLayout.CENTER);
     cp.add (panel, BorderLayout.NORTH);
     cp.add (button, BorderLayout.SOUTH);
-    
-    /*         
-    t_pps.setBackground ( Color.lightGray );
-    messageLog.setBackground ( Color.lightGray );
-    t_pktloss.setBackground ( Color.lightGray );
-    t_pktrcvd.setBackground ( Color.lightGray );
-    */
 
     pack();
     setVisible(true);
 
     RED = new Color(255, 158, 158);
     GREEN = new Color(125, 249, 150);
-   
-    
+       
   }
 
-  // Timer thread to count PPS and packet loss at interval of 1 second
-  class Schedule extends TimerTask
-  {
-    public void run()
-    { 
-       /*
-       t_pps.setText(Integer.toString(pps)); 
-       t_pktloss.setText(Integer.toString(totalPktLost)); 
-       t_pktrcvd.setText(Integer.toString(totalPktRcvd)); 
-
-       // System.out.println(pps + " pps, Total packet lost:" + totalPktLost);
-       // System.out.println("Seq no: " + check_seqno + ", " + seqno);
-       if(pps == 0 && check_seqno !=0)
-       {
-         if (receiving)
-         {
-           t_pps.setBackground ( RED );
-           messageLog.setBackground ( RED );
-           t_pktloss.setBackground ( RED );
-           t_pktrcvd.setBackground ( RED );
-           Calendar now = Calendar.getInstance();
-           log += "Timeout at: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
-           messageLog.setText(log);
-         }
-       }
-       else if (OUT_SEQ)
-       {
-         t_pktloss.setBackground ( RED );
-         messageLog.setBackground ( RED );
-         Calendar now = Calendar.getInstance();
-         log += "Incorrect sequence: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
-         log += "From " + check_seqno + " to " + seqno +"\n";
-         messageLog.setText(log);
-       }
-       else if (check_seqno!=0)
-       {
-         t_pps.setBackground ( GREEN );
-         messageLog.setBackground ( GREEN );
-         t_pktrcvd.setBackground ( GREEN );
-         if (totalPktLost==0) t_pktloss.setBackground ( GREEN );
-       }
-       pps=0;
-       if (!receiving)
-       {
-         t_pps.setBackground ( Color.lightGray );
-         messageLog.setBackground ( Color.lightGray );
-         t_pktloss.setBackground ( Color.lightGray );
-         t_pktrcvd.setBackground ( Color.lightGray );
-       }*/
-     } //End run()
-   }
-
-   class Rpkt extends TimerTask
-   {
-      
-      Rpkt()
-      {
-        // p = new DatagramPacket(buf, buf.length);
-      }
-
-      public void run()
-      {
-         try
-         {
-           // Continue keep receive packets, corresponding flags updated by timer thread
-            if (receiving)
-            {
-             /*
-             buf = new byte[IP_PKT_SIZE_MAX];
-             //p = new DatagramPacket(buf, buf.length);
-             p.setData(buf);
-             s.receive(p);
-
-             // System.out.println("Packet length:" + p.getLength());
-              
-             // Extract sequence number from newly arrived packet
-             ByteArrayInputStream bi = new ByteArrayInputStream(buf);
-             DataInputStream di = new DataInputStream(bi);
-             seqno = di.readInt();
-
-             // Account for lost packet
-             if(check_seqno <= 0 && seqno > 0)
-             {
-               check_seqno = seqno;
-             }
-             else if(check_seqno !=0 && seqno - check_seqno != 1)
-             {
-              // System.out.println("Lost packet:" + (seqno - check_seqno));
-              if (seqno > check_seqno)
-              {
-               totalPktLost += seqno - check_seqno - 1;
-               check_seqno = seqno;
-               t_pktloss.setBackground ( RED );
-              }
-              else if (seqno < check_seqno)
-              {
-                t_pktrcvd.setBackground ( RED );
-                messageLog.setBackground ( RED );
-                OUT_SEQ = true;
-                Calendar now = Calendar.getInstance();
-                log += "Incorrect sequence: " + now.get(Calendar.DAY_OF_MONTH) + "-" + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.YEAR) + " " + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":" + now.get(Calendar.SECOND) + "\n";
-                log += "From " + check_seqno + " to " + seqno +"\n";
-                messageLog.setText(log);
-              }
-             }
-             else if(seqno - check_seqno == 1)
-             {
-               check_seqno = seqno;
-               t_pktrcvd.setBackground( GREEN );
-               messageLog.setBackground( GREEN );
-               OUT_SEQ = false;
-             }
-             totalPktRcvd++;
-             pps++;*/
-            }
-        } catch (Exception e){ System.out.println (e); }
-      }
-   }
  
    public static void main(String[] args)
    {
--- simpleTCPserver.java
+++ simpleTCPserver.java
@@ -20,29 +20,15 @@ import static java.lang.System.out;
 public class simpleTCPserver extends JFrame
 {
   private java.util.Timer timer;
-  private static final int IP_PKT_SIZE_MAX = 65000;
-  private static int seqno = 0;
-  private static int packet_length = 0;
   private static int port = 5000;
-  private static int ttl = 15;
-  private static int pps = 10;
-  private static String group = "239.10.10.1";
   private static boolean sending = false;
-  private static double packet_gap = 0.0;
-  private static double gap = 0.0;
-  private static int timer_count = 0;
  
-  private JTextField p_size, p_port, p_group, p_rate, p_ttl;
+  private JTextField p_port;
   private JTextArea messageLog;
   private JButton start, stop;
-  private JLabel l_size, l_port, l_group, l_rate, l_ttl, l_int;
+  private JLabel l_port, l_group;
   private JPanel panel, button;
-  private JComboBox n_interface;
 
-  /* 
-  private static MulticastSocket s;
-  private static NetworkInterface netint;
-  */
   private static ServerSocket welcomeSocket;
   private static Socket connectionSocket;
   private static BufferedReader inFromClient;
@@ -52,23 +38,6 @@ public class simpleTCPserver extends JFrame
     super ("Simple TCP Server 1.0");
     setIconImage( new ImageIcon("mcast.gif").getImage());
 
-    String[] netString = {""};
-    n_interface = new JComboBox(netString);
-
-    // List out available Network Interfaces
-    /*
-    try
-    {
-      Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
-      for (NetworkInterface netint : Collections.list(nets))
-      {
-        //out.printf("Name: %s\n", netint.getName());
-        n_interface.addItem(netint.getName());
-      }
-    } catch  ( Exception ex )
-    {
-    }
-    */
     class ButtonListener implements ActionListener
      {
         private String t;
@@ -85,40 +54,13 @@ public class simpleTCPserver extends JFrame
             start.setEnabled(false);
             stop.setEnabled(true);
             port = Integer.parseInt(p_port.getText());
-            //pps = Integer.parseInt(p_rate.getText());
-            //ttl = Integer.parseInt(p_ttl.getText());
-            //packet_length = Integer.parseInt(p_size.getText());
-            // group = p_group.getText();
+
             if (port < 1025 || port > 65535) 
             { if (port < 1025) { port = 1025; p_port.setText("1025");}
               else { port = 65535; p_port.setText("65535");} }
-            /*if (pps < 0 || pps > 1000000) 
-            { if (pps < 0) { pps = 0; p_rate.setText("0");}
-              else { pps = 1000000; p_rate.setText("1000000");} }
-            if (ttl < 1 || ttl > 127) 
-            { if (ttl < 1) { ttl = 1; p_ttl.setText("1");}
-              else { ttl = 127; p_ttl.setText("127");} }
-            if (packet_length < 4 || packet_length > IP_PKT_SIZE_MAX) 
-            { if (packet_length < 4){ packet_length = 4; p_size.setText("4");}
-              else { packet_length = IP_PKT_SIZE_MAX; p_size.setText((new Integer(IP_PKT_SIZE_MAX)).toString());} }
-            packet_gap = 1000.0/(double)pps;
-            timer_count = 0;
-            */
+
             try
             {
-              /*
-              s = new MulticastSocket(port);
-              Enumeration<NetworkInterface> nic = NetworkInterface.getNetworkInterfaces();
-               for (NetworkInterface nc : Collections.list(nic))
-               {
-                 String x = String.valueOf(n_interface.getSelectedItem());
-                 if ( x.equals (nc.getName()))
-                 {
-                   out.printf ("Interface: %s\n", x);
-                   s.setNetworkInterface(nc);
-                 }
-               }
-               */
                welcomeSocket = new ServerSocket(port);
                connectionSocket = welcomeSocket.accept();
                sending = true;
@@ -163,33 +105,12 @@ public class simpleTCPserver extends JFrame
 
      panel = new JPanel ( new GridLayout(1,2) );
 
-     // l_group = new JLabel ("Multicast Group");
-     // p_group = new JTextField(group);
      l_port = new JLabel("TCP port");
      p_port = new JTextField("5000");
-     /*
-     l_size = new JLabel("Packet size (payload) ");
-     p_size = new JTextField("256");
-     l_rate = new JLabel("Rate (pps) ");
-     p_rate = new JTextField("10");
-     l_ttl = new JLabel("TTL");
-     p_ttl = new JTextField("15");
-     l_int = new JLabel("Interface");
-     */
-     // panel.add(l_group);
-     // panel.add(p_group);
+
      panel.add(l_port);
      panel.add(p_port);
-     /*
-     panel.add(l_size);
-     panel.add(p_size);
-     panel.add(l_rate);
-     panel.add(p_rate);
-     panel.add(l_ttl);
-     panel.add(p_ttl);
-     panel.add(l_int);
-     panel.add(n_interface);
-     */
+
      button = new JPanel();
      button.setLayout( new FlowLayout(FlowLayout.CENTER) );
 
@@ -221,12 +142,9 @@ public class simpleTCPserver extends JFrame
 
   class Schedule extends TimerTask
   {
-    // byte[] buf;
-    // DatagramPacket p;
 
     public void run()
     {
-      //timer_count++;
 
      if(sending)
      {
@@ -242,45 +160,9 @@ public class simpleTCPserver extends JFrame
 
            messageLog.setText(clientSentence);
          }
-        /*
-        byte [] seqbuf = new byte[4];
-        buf = new byte[packet_length];
 
-        ByteArrayOutputStream bo = new ByteArrayOutputStream(packet_length);
-        DataOutputStream doutput = new DataOutputStream(bo);
-
-        p = new DatagramPacket(buf, buf.length, InetAddress.getByName(group), port);
-
-        // System.out.println(seqno + " " + gap + " " + packet_gap + " " + timer_count);
-        while (gap < timer_count) 
-        {
-          // Fill up packet with arbitary data
-          // for (int i=0; i<packet_length; i++) buf[i] = (new Integer(i)).byteValue();
-
-          seqno++;
-
-          // Convert sequence number to a 4-byte array
-          doutput.writeInt(seqno);
-          seqbuf = bo.toByteArray();
-
-          // Insert sequence number to the buffer
-          buf[0] = seqbuf[0];
-          buf[1] = seqbuf[1];
-          buf[2] = seqbuf[2];
-          buf[3] = seqbuf[3];
-
-          // Create packet and send out
-          //p = new DatagramPacket(buf, buf.length, InetAddress.getByName(group), port);
-          p.setData(buf);
-          
-          s.setTimeToLive((byte)ttl);
-          s.send(p);
-          gap += packet_gap;
-        }
-        */
        } catch (Exception e){}
       }
-      // if (timer_count == 1000) { timer_count = 0; gap = 0.0; }
     } // End run()
   }
 
